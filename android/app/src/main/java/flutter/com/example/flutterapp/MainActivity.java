12b16edb7affe759a3c6b79c93bda785e32a4725
yancheng <yanchengdeng@gmail.com>
2018-12-29 16:08:21 +0800

    发布第一个master1.0

---
+++ android/app/src/main/java/flutter/com/example/flutterapp/MainActivity.java
@@ -0,0 +1,17 @@
+package flutter.com.example.flutterapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+
+    //发布第一个master 1.0 版本
+
+
+  }
+}
